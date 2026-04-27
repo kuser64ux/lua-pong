35f3a9b26b09cfbd8dde5d1aea0e59e75d082eee
kuser <kuser64ux@gmail.com>
2026-04-27 14:35:31 +0300

    finale

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- Add an Enemy AI
+
 push = require "push"
 
 Class = require "class"
@@ -22,31 +24,49 @@ function love.load()
     math.randomseed(os.time())
 
     smallFont = love.graphics.newFont("font.ttf", 8)
+    largeFont = love.graphics.newFont('font.ttf', 16)
     scoreFont = love.graphics.newFont("font.ttf", 32)
-
     love.graphics.setFont(smallFont)
 
+    sounds = {
+        ["paddle_hit"] = love.audio.newSource("sounds/paddle_hit.wav", "static"),
+        ["score"] = love.audio.newSource("sounds/score.wav", "static"),
+        ["wall_hit"] = love.audio.newSource("sounds/wall_hit.wav", "static")
+    }   
+
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        resizable = false,
-        vsync = true,
+        resizable = true,
         fullscreen = false
+        vsync = true,
     })
 
-    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = "normal"})
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = "normal" })
 
     player1Score = 0
     player2Score = 0
 
+    servingPlayer = 1
+
     player1 = Paddle(10, 30, 5, 20)
     player2 = Paddle(VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 35, 5, 20)
-
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = "start"
 end
 
+function love.resize(width, height)
+    push.resize(width, height)
+end
+
 function love.update(dt)
-    if gameState == "play" then
+    if gameState == "serve" then
+        ball.dy = math.random(-50, 50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = -math.random(140, 200)
+        end
+    elseif gameState == "play" then
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.03
             ball.x = player1.x + 5
@@ -56,6 +76,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds["paddle_hit"]:play()
         end
 
         if ball:collides(player2) then
@@ -67,16 +89,53 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds["paddle_hit"]:play()
         end
 
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+
+            sounds["wall_hit"]:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+
+            sounds["wall_hit"]:play()
+        end
+
+        if ball.x < 0 then
+            servingPlayer = 1
+            player2Score = player2Score + 1
+
+            sounds["score"]:play()
+
+            if player2Score == 2 then
+                winningPlayer = 2
+                gameState = "done"
+            else
+                gameState = "serve"
+                ball:reset()
+            end
+        end
+
+        if ball.x > VIRTUAL_WIDTH then
+            servingPlayer = 2
+            player1Score = player1Score + 1
+
+            sounds["score"]:play()
+
+            if player1Score == 2 then
+                winningPlayer = 1
+                gameState = 'done'
+            else
+                gameState = "serve"
+                ball:reset()
+            end
+            
         end
     end
 
@@ -109,11 +168,22 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == "enter" or key == "return" then
         if gameState == "start" then
+            gameState = "serve"
+        elseif gameState == "serve" then
             gameState = "play"
-        else
-            gameState = start
+        elseif gameState == "done" then
+            gameState = "serve"
 
             ball:reset()
+
+            player1Score = 0
+            player2Score = 0
+
+            if winningPlayer == 1 then
+                servingPlayer = 2
+            else
+                servingPlayer = 1
+            end
         end
     end
 end
@@ -123,11 +193,29 @@ function love.draw()
 
     love.graphics.clear(40/255, 45/255, 52/255, 1) -- Background color
 
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
+    love.graphics.setFont(smallFont)
+
+    displayScore()
+
+    if gameState == "start" then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf("Welcome to Pong!", 0, 10, VIRTUAL_WIDTH, "center")
+        love.graphics.printf("Press Enter to begin!", 0, 20, VIRTUAL_WIDTH, "center")
+    elseif gameState == "serve" then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf("Player" .. tostring(servingPlayer) .. "s serve!", 
+            0, 10, VIRTUAL_WIDTH, "center")
+        love.graphics.printf("Press Enter to serve!", 0, 20, VIRTUAL_WIDTH, "center")
+    elseif gameState == "play" then
+        -- no UI messages to display in play
+    elseif gameState == "done" then
+        -- UI messages
+        love.graphics.setFont(largeFont)
+        love.graphics.printf('Player ' .. tostring(winningPlayer) .. ' wins!',
+            0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Press Enter to restart!', 0, 30, VIRTUAL_WIDTH, 'center')
+    end
 
     player1:render()
     player2:render()
@@ -144,4 +232,12 @@ function displayFPS()
     love.graphics.setColor(0, 1, 0, 1)
     love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
     love.graphics.setColor(1, 1, 1, 1)
+end
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
+        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 3)
 end
